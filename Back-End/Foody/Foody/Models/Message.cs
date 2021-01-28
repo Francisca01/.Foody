068fe041dc243ebd7575947131256c36796dac1f
Leonardo Vieira <a18464@alunos.ipca.pt>
2021-01-28 14:24:04 +0000

    fix merge

---
+++ Back-End/Foody/Foody/Models/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foody.Models
+{
+    // Classe de Encomenda - Produto
+    public class Message
+    {
+        public string text { get; set; }
+
+        public Message()
+        {
+
+        }
+    }
+}
