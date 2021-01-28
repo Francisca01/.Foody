5cedff7cc82e37f0e03b1807c378d8a16f0f5a63
Leonardo Vieira <a18464@alunos.ipca.pt>
2021-01-28 14:36:11 +0000

    fix back-end

---
+++ Back-End/Foody/Foody/Controllers/CompaniesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Foody.Models;
+using Foody.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Foody.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        // GET: api/<CompaniesController>
+        [HttpGet]
+        public List<object> Get()//so pode ser acedido pelo admin
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+            if (token != null)
+            {
+                //vai buscar os utilizadores
+                return UserService.GetUser(token, 2);
+            }
+            else
+            {
+                List<object> msg = new List<object>() { MessageService.AccessDenied() };
+                return msg;
+            }
+        }
+
+        // GET api/<CompaniesController>/5
+        [HttpGet("{idUser}")]
+        public object Get(int idUser)
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+            if (token != null)
+            {
+                //vai buscar o utilizadore
+                return UserService.GetUserId(token, idUser);
+            }
+            else
+            {
+                return MessageService.AccessDenied();
+            }
+        }
+        
+        // PUT api/<CompaniesController>/5
+        [HttpPut("{idUser}")]
+        public Message Put(int idUser, [FromBody] User companyUpdate)
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+            if (token != null)
+            {
+                return UserService.PutUser(token, companyUpdate, idUser);
+            }
+            else
+            {
+                return MessageService.AccessDenied();
+            }
+        }
+
+        // DELETE api/<CompaniesController>/5
+        [HttpDelete("{idUser}")]
+        public Message Delete(int idUser)
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+            if (token != null)
+            {
+                return UserService.DeleteUser(token, idUser);
+            }
+            else
+            {
+                return MessageService.AccessDenied();
+            }           
+        }
+    }
+}
---
+++ Back-End/Foody/Foody/Controllers/DeliveriesController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Foody.Models;
+using Foody.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Foody.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class DeliveriesController : ControllerBase
+    {
+        // GET: api/<DeliveriesController>
+        [HttpGet]
+        public List<object> Get()
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+
+            int[] userLoggedIn = UserService.UserLoggedIn(token);
+
+            if (userLoggedIn != null)
+            {
+                return DeliveryService.GetDeliveries(userLoggedIn);
+            }
+            else
+            {
+                List<object> msg = new List<object>() { MessageService.AccessDenied() };
+                return msg;
+            }
+        }
+
+        // GET api/<DeliveriesController>/5
+        [HttpGet("{idDelivery}")]
+        public object Get(int idDelivery)
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+
+            if (token != null)
+            {
+                return DeliveryService.GetDeliveryId(token, idDelivery);
+            }
+            else
+            {
+                List<object> msg = new List<object>() { MessageService.AccessDenied() };
+                return msg;
+            }            
+        }
+
+        // PUT api/<DeliveriesController>/5
+        [HttpPut("{idDelivery}")]
+        public Message Put(int idDelivery, [FromBody] Delivery deliveryUpdate)
+        {
+            //token do user logado
+            string token = Request.Headers["token"];
+
+            int[] userLoggedIn = UserService.UserLoggedIn(token);
+
+            if (userLoggedIn != null)
+            {
+                return DeliveryService.ChangeDeliveryState(userLoggedIn[0], deliveryUpdate, idDelivery);
+            }
+            else
+            {
+                return MessageService.AccessDenied();
+            }
+        }
+
+        //NOTA: Podem ser eliminadas, mas após delivery ser concluida
+        // Adicionar um status à delivery, quando concluido, eliminada
+
+
+        // Por questões de política e privacidade de dados: Entregas não podem ser eliminadas
+    }
+}
